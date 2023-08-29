b24340a548474f868277b4a633b0b8fbee1d0c02
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-08-29 20:41:07 +0500

    Api - [08]

--- src/app/api_integerate/page.tsx
+++ src/app/api_integerate/page.tsx
@@ -1,33 +1,30 @@
-const getBooksData = async () => {
+const getData = async () => {
   const res = await fetch("https://simple-books-api.glitch.me/books/", {
     method: "GET",
-    cache : "no-store",
+    cache: "no-store",
   });
   console.log(res);
-  if (!res.ok) {
-    console.log("Failed error to fetch Data");
-
-    const data = await res.json();
+  const data = await res.json();
     return data;
-  }
-};
 
-interface IBook {
-  id: number;
-  name: string;
-  type: string;
-  avaliable: boolean;
+  }
+
+
+// interface IBook {
+//   id: number;
+//   name: string;
+//   type: string;
+//   avaliable: boolean;
+// }
+
+interface IQuote {
+    content: string,
 }
 
 export default async function Home() {
-  const data: IBook[] = await getBooksData();
-  return (
-    <>
-      {data.map((book) => {
-        return (
-            <h1 key={book.id}>{book.name}</h1>
-        )
-      })}
-    </>
-  );
+  //   const data: IBook[] = await getBooksData();
+  const quote:IQuote = await getData();
+  console.log(quote);
+  
+  return <div>{quote.content}</div>;
 }
